235ad73fc203048d96ae03c13edc467be8798a06
adesanyaolu <sanya4reel@gmail.com>
2023-11-19 22:06:53 +0100

    Suspending the project

--- t_connect_v/src/components/HeaderHead.vue
+++ t_connect_v/src/components/HeaderHead.vue
@@ -1,9 +1,33 @@
 
+<script setup>
+import { ref } from 'vue'
+
+let text = ref('')
+
+function onClick (){
+    text.value = text
+    console.log(text)
+}
+
+</script>
+
+
 <template>
-    <div>
+
         <h1 class="text-3xl font-bold underline">
             Hello world!
         </h1>
-  </div>
+       
+            <input  placeholder="Type here">
+            <button @click="onClick" >Submit</button>
+            <p>{{ text }}</p>
+
 
 </template>
+
+<style>
+p {
+    color: red;
+}
+
+</style>
